f192f29d019dddcf129b68ca42ca63760edeec76
Sagar Bhakat <sagarbhakat12345@gmail.com>
2025-01-10 19:55:36 +0530

    added movement in y direction also & a blur div is added too.

--- script.js
+++ script.js
@@ -1,7 +1,16 @@
-window.addEventListener("mousemove", function(details) {
+let blr = document.querySelector("#box2");
+document.addEventListener("mousemove", function(details) {
+    blr.style.left = details.x + "px";
+    blr.style.top = details.y + "px";
+
+})
+document.addEventListener("mousemove", function(details) {
+
 
     gsap.to("#box", { //gsap concepts(methods) chatGPT se uthaye hai
         left: details.clientX + "px",
-        ease: Power3,
+        top: details.clientY + "px",
+        duration: 0.5,
+        ease: "Power2.easeOut",
     });
 });
